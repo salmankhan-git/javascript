f4c1d46891b1514c68ded4cd6195f58f9df6e1dd
salmankhan-git <salmankhan.techky@gmail.com>
2025-08-03 23:33:42 +0530

    Removed JS submodule and added it as a normal folder

---
+++ JS/01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 12453
+let accountEmail = "salmankhan@gmail.com"
+var accountPassword = "5894"
+accountCity= "jaipur"
+let accountState;
+
+// const cant be changed
+// var is not preferred bcz of issue in block scope and function scope
+
+// console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountState, accountCity]);
---
+++ JS/01_basics/1.js
@@ -0,0 +1,6 @@
+const str1 ="hello world"
+console.log(str1.split("").reverse().join(""))
+
+
+// number=100
+// console.log(Math.round(4.9));
