28d2f73f9fad048a5b87d480d1bef69c7a94f161
salmankhan-git <salmankhan.techky@gmail.com>
2025-08-12 18:56:41 +0530

    changes and datatypes

--- JS/01_basics/03_conversionOperation.js
+++ JS/01_basics/03_conversionOperation.js
@@ -34,3 +34,35 @@ console.log(stringNumber);
 console.log(typeof stringNumber)
 
 
+
+// **************** Operations ***************************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "salman"
+
+let str3 = str1 + str2;
+                            //      Output
+console.log("1"+2)          //      12
+console.log("1"+"2")        //      12
+console.log(1+"2")          //      12
+console.log("1"+2+2)        //      122
+console.log(2+1+"2")        //      32
+
+console.log(+true)          //      1
+console.log(+"")            //      0
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
---
+++ JS/01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0);
+
+// comparison operator (> < >= <=)here convert null as 0 whereas == does not
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<=0);
+console.log(undefined!=0);  //true
+console.log(null!=0);
+
+
+// strict check ===
+
+console.log("2"===2)
+
