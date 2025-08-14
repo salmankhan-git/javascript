3bed583c8769c85986bf14bf53bd3d8c18746284
salmankhan-git <salmankhan.techky@gmail.com>
2025-08-14 17:03:34 +0530

    changes

--- JS/01_basics/datatypes-summary.js
+++ JS/01_basics/datatypes-summary.js
@@ -35,3 +35,37 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++
+
+
+// stack(primitive)   heap(non-primitve)
+/*
+stack ke andar joh bhi store hota hai uski sidha value store hoti hai 
+or agar ham us variable ko kisi or variable se assign karte hain toh bas usse variable ki copy kardeta hai 
+or agar haam uske baad naye variable mein changes kare toh purne wale koh kuch nahi hoga
+naya wale badalta hai
+
+wahi heap mein variable toh stack mein hota hai par data sara heap mein hota hai
+jis ko woh refer kar raha hota hai agar ham is variable ko kis or variable se assign karte hain
+toh naya variable bhi ushi data ko refer karta hai na ki koi naya copy nbanti hai jis ki 
+wajah se agar ham naye variable mein changes kare toh  original bhi change hojaye ga
+*/
+
+let myYoutubeName="sk"
+
+let myAnotherChannel = myYoutubeName 
+
+myAnotherChannel="salman"
+console.log(myYoutubeName);
+console.log(myAnotherChannel);
+
+let userOne={
+    email:"salman@google.com",
+    upi:"ambani@ypl"
+}
+let userTwo= userOne
+userTwo.email="ambani@mail.com"
+
+console.log(userOne);
+console.log(userTwo);
