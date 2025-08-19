fe747701149c8c259b948bd8969d933593975b9e
salmankhan-git <salmankhan.techky@gmail.com>
2025-08-19 23:49:00 +0530

    changes

--- JS/01_basics/07_dates.js
+++ JS/01_basics/07_dates.js
@@ -1,5 +1,5 @@
 // DATE
-
+/*
 let myDates = new Date();
                                                             // OUTPUT        
  console.log(myDates);                                     //2025-08-17T17:47:17.819Z
@@ -10,8 +10,28 @@ let myDates = new Date();
  console.log(myDates.toLocaleDateString());                //17/8/2025
  console.log(myDates.toTimeString());                      //23:17:17 GMT+0530 (India Standard Time)                         
  
-//  let myCreatedDate = new Date(2025, 0, 25)
+//  let myCreatedDate = new Date(2025, 0, 25)  //is mein month 0 se start hai
 //  let myCreatedDate = new Date(2025, 0, 25,5,46)
 //  let myCreatedDate = new Date("2025-01-14")
+
+*/
  let myCreatedDate = new Date("08-24-2004")
   console.log(myCreatedDate.toLocaleString()); 
+
+  let myTimeStamp = Date.now() // yeh date.now current date im millisecond from default date
+  console.log(myTimeStamp);
+  console.log(myCreatedDate.getTime()); // .gettime bhi date bata raha hai in ms from given value
+  console.log(Math.round(Date.now()/1000)); // yeh second mein hain
+  
+  let newDate = new Date()
+  console.log(newDate);
+  console.log(newDate.getMonth());  // is mein +1 isliye kyoki date mein month 0 se start hota hai 
+  console.log(newDate.getDay());
+
+  // isko normally ese use karte hai
+  //   `${newDate.getDay()} and the time....`
+
+  newDate.toLocaleString('default',{
+    weekday:"long",
+    })
+  
