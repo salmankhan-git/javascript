833fe10cdb5ed16626ecb60d70054868c9e7ecb1
salmankhan-git <salmankhan.techky@gmail.com>
2025-08-27 19:18:28 +0530

    iife

--- JS/03_basics/04_iife.js
+++ JS/03_basics/04_iife.js
@@ -4,12 +4,14 @@
 
 // syntax  ()()  // in this the first parenthesis is for funtion and the second one is for exection
 
+// named  iife 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
 })();
 // in iife it is must to use semicolon after the line end bcz iife invokes the function but it does not tell where to stop. 
 
+// simple or un-named iife
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
