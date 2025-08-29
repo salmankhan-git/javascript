c73b76cfe0d16e90a3ef8f34d812e7070dc3306a
salmankhan-git <salmankhan.techky@gmail.com>
2025-08-29 23:33:22 +0530

    Iterator-do_while loop

--- JS/05_iterations/02_while_&_do-while_loop.js
+++ JS/05_iterations/02_while_&_do-while_loop.js
@@ -1,3 +1,4 @@
+// while loop
 
 let index = 0
 // while (index <= 10) {
@@ -5,6 +6,7 @@ let index = 0
 //     index = index + 2
 // }
 
+// how we use it in array
 let myArray = ['flash', "batman", "superman"]
 
 let arr = 0
@@ -13,9 +15,14 @@ while (arr < myArray.length) {
     arr = arr + 1
 }
 
+
+// do-while loop
+// in this loop we first do the operation then check the condition on the first iteration.
+
 let score = 11
 
 do {
     console.log(`Score is ${score}`);
     score++
 } while (score <= 10);
+// as we said above it will print the do block once after checking it will stop
